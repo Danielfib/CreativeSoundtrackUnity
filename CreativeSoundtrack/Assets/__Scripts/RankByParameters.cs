8c749895a3405f1dad686ab57e239f0ee4b865d0
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-11-04 00:29:41 -0300

    Including Spotify4Unity and starting audio features sort

---
+++ CreativeSoundtrack/Assets/__Scripts/RankByParameters.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankByParameters : MonoBehaviour
+{
+    [Range(0,1), SerializeField]
+    private float loudness, energy, instrumentalness, speechiness;
+
+    public void DoRank()
+    {
+        GameObject.FindObjectOfType<ExampleTracksController>().SortByFeatures(loudness, energy, instrumentalness, speechiness);
+    }
+}
