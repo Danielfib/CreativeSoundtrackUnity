2c4565d81ebd6711903a50851173035cda8bf688
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-11-19 00:19:31 -0300

    Starting custom editor script for SoundtrackArea

---
+++ CreativeSoundtrack/Assets/Editor/SoundtrackAreaCustomEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(SoundtrackArea))]
+public class SoundtrackAreaCustomEditor : Editor
+{
+    public Texture2D battleVibeIcon;
+    public Texture2D romanticVibeIcon;
+
+    const int ICON_HEIGHT = 40;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUILayout.Space(20f);
+        GUILayout.Label("Trocar entre vibes", EditorStyles.boldLabel);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button(battleVibeIcon, GUILayout.Width(ICON_HEIGHT), GUILayout.Height(ICON_HEIGHT)))
+        {
+            Debug.Log("battle: " + target.name);
+        }
+
+        if (GUILayout.Button(romanticVibeIcon, GUILayout.Width(ICON_HEIGHT), GUILayout.Height(ICON_HEIGHT)))
+        {
+            Debug.Log("romantic: " + target.name);
+        }
+
+        GUILayout.EndHorizontal();
+    }
+}
