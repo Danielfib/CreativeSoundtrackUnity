35712b3b5f6c238c9a8289eb8f428d2dc6468f1a
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-11-24 23:25:53 -0300

    Including icons and information buttons

--- CreativeSoundtrack/Assets/Editor/SoundtrackAreaCustomEditor.cs
+++ CreativeSoundtrack/Assets/Editor/SoundtrackAreaCustomEditor.cs
@@ -17,16 +17,28 @@ public class SoundtrackAreaCustomEditor : Editor
     int toolbarIntAux = -1;
     int toolbarInt = 0;
 
+    private static readonly string[] _dontIncludeMe = new string[] { "m_Script" };
+
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
+        //hide Script property
+        serializedObject.Update();
+        DrawPropertiesExcluding(serializedObject, _dontIncludeMe);
+        serializedObject.ApplyModifiedProperties();
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Open Source Code"))
+            Application.OpenURL("https://github.com/Danielfib/CreativeSoundtrackUnity/tree/main/CreativeSoundtrack");
+        if (GUILayout.Button("Open Spotify Dashboard"))
+            Application.OpenURL("https://developer.spotify.com/dashboard/");
+        GUILayout.EndHorizontal();
 
         GUILayout.Space(20f);
         GUILayout.Label("Trocar entre vibes", EditorStyles.boldLabel);
 
         Texture2D[] textures = { battleVibeIcon, romanticVibeIcon, exploringVibeIcon, sadVibeIcon };
         toolbarInt = GUILayout.Toolbar(toolbarInt, textures, GUILayout.Height(ICON_HEIGHT));
-        if(toolbarIntAux != toolbarInt)
+        if (toolbarIntAux != toolbarInt)
         {
             SoundtrackArea targetSA = (SoundtrackArea)target;
             toolbarIntAux = toolbarInt;
